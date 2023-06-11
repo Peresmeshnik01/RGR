84fc4ba19978060f22658416a7f57bf406cdfec0
vldpvnnk <133138277+vldpvnnk@users.noreply.github.com>
2023-06-11 15:18:59 +0700

    Update 5.6.cpp

--- 5.6/5.6.cpp
+++ 5.6/5.6.cpp
@@ -37,79 +37,95 @@ string decrypt(string text, int k, int* perm) {
         string group = text.substr(start, end - start);
         string decrypted_group = "";
         for (int j = 0; j < k; j++) {
-            decrypted_group += group[find(perm, perm+k, j+1) - perm];
+            decrypted_group += group[find(perm, perm + k, j + 1) - perm];
         }
         result += decrypted_group;
     }
     return result;
 }
-string get_password() { 
-    string password; 
-    cout << "Введите пароль: "; 
-    cin >> password; 
-    return password; 
+string get_password() {
+    string password;
+    cout << "Введите пароль: ";
+    cin >> password;
+    return password;
 }
 
-void write_to_file(string filename, string text) { 
-    ofstream file(filename); 
-    file << text; 
-    file.close(); 
-    cout << "Текст записан в файл " << filename << endl; 
-} 
- 
-string read_from_file(string filename) { 
-    ifstream file(filename); 
-    string text((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>())); 
-    file.close(); 
-    return text; 
-} 
+void write_to_file(string filename, string text) {
+    ofstream file(filename);
+    file << text;
+    file.close();
+    cout << "Текст записан в файл " << filename << endl;
+}
+
+string read_from_file(string filename) {
+    ifstream file(filename);
+    string text((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+    file.close();
+    return text;
+}
+
+bool isEnglish(string str) {
+    for (int i = 0; i < str.length(); i++) {
+        int asciiCode = (int)str[i];
+        if (asciiCode < 65 || (asciiCode > 90 && asciiCode < 97) || asciiCode>122) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
+    setlocale(LC_ALL, "RUS");
     int k = 4;
-    int perm[] = {3, 2, 4, 1};
+    int perm[] = { 3, 2, 4, 1 };
     string plaintext;
     cout << "Введите исходный текст: ";
-    getline(cin,plaintext);
+    getline(cin, plaintext);
     char answer;
-    write_to_file("source.txt", plaintext);
-    string password = get_password();
-    // шифрование   
-    if (password == "secret") {
-        plaintext = read_from_file("source.txt");
-        plaintext = encrypt(plaintext, k, perm);
-        write_to_file("encoded_plaintext.txt", plaintext);
-        cout << "Текст зашифрован и записан в encoded_plaintext.txt" << endl;
-    }
-    else {
-        cout << "Неверный пароль, шифрование невозможно" << endl;
-    }
+    bool mes = isEnglish(plaintext);
+    if (mes) {
+        write_to_file("source.txt", plaintext);
+        string password = get_password();
+        // шифрование   
+        if (password == "secret") {
+            plaintext = read_from_file("source.txt");
+            plaintext = encrypt(plaintext, k, perm);
+            write_to_file("encoded_plaintext.txt", plaintext);
+            cout << "Текст зашифрован и записан в encoded_plaintext.txt" << endl;
+        }
+        else {
+            cout << "Неверный пароль, шифрование невозможно" << endl;
+        }
 
-    password = get_password();
-    // дешифрование   
-    if (password == "secret") {
-        plaintext = read_from_file("encoded_plaintext.txt");
-        plaintext = decrypt(plaintext, k, perm);
-        write_to_file("decoded_plaintext.txt", plaintext);
-        cout << "Текст расшифрован и записан в decoded_plaintext.txt" << endl;
+        password = get_password();
+        // дешифрование   
+        if (password == "secret") {
+            plaintext = read_from_file("encoded_plaintext.txt");
+            plaintext = decrypt(plaintext, k, perm);
+            write_to_file("decoded_plaintext.txt", plaintext);
+            cout << "Текст расшифрован и записан в decoded_plaintext.txt" << endl;
+        }
+        else {
+            cout << "Неверный пароль, дешифрование невозможно" << endl;
+        }
+        cout << "Нажмите 'p', чтобы распечатать файлы: ";
+        cin >> answer;
+        if (answer == 'p' || answer == 'P') {
+            cout << "Файл source.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("source.txt") << endl;
+            cout << endl;
+            cout << "Файл encoded_plaintext.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("encoded_plaintext.txt") << endl;
+            cout << endl;
+            cout << "Файл decoded_plaintext.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("decoded_plaintext.txt") << endl;
+        }
     }
-    else {
-        cout << "Неверный пароль, дешифрование невозможно" << endl;
+    else if (!mes) {
+        cout << "Сообщение написано на русском языке";
     }
-    cout << "Нажмите 'p', чтобы распечатать файлы: ";
-    cin >> answer;
-    if (answer == 'p' || answer == 'P') {
-        cout << "Файл source.txt:" << endl;
-        cout << endl;
-        cout << read_from_file("source.txt") << endl;
-        cout << endl;
-        cout << "Файл encoded_plaintext.txt:" << endl;
-        cout << endl;
-        cout << read_from_file("encoded_plaintext.txt") << endl;
-        cout << endl;
-        cout << "Файл decoded_plaintext.txt:" << endl;
-        cout << endl;
-        cout << read_from_file("decoded_plaintext.txt") << endl;
-    }
-    
     return 0;
 }
